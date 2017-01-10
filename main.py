3a3a6ae16bb0bc4018187257ca7c20ef6814d757
Paul Bradshaw <paulonhismobile@gmail.com>
2017-01-10 22:41:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Hello World program in Python
+    
+print "Hello World!\n"
+# Hello World program in Python
+import urllib, json
+url = "https://data.police.uk/api/forces"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+#print data
+for i in data:
+    print i
+    print i['name']
+    if i['id'] == 'wiltshire':
+        print 'THIS IS WILTSHIRE2'
+    if 'wilt' in i['id']:
+        print 'CONTAINS WILT'
+
+f = open('workfile', 'w')
