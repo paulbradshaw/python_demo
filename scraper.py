95fad47117462874f8ce7ad7f14907ea24779315
Paul Bradshaw <paulonhismobile@gmail.com>
2017-01-10 23:11:07 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,2 +1,24 @@
-import sqlite3
-connection = sqlite3.connect("company.db")
+#import sqlite3
+#connection = sqlite3.connect("company.db")
+
+import scraperwiki
+import lxml.html
+import urllib, json
+
+record = {}
+
+url = "https://data.police.uk/api/forces"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+#print data
+for i in data:
+    print i
+    print i['name']
+    record['idcode'] = i['id']
+    record['fullname'] = i['name']
+    if i['id'] == 'wiltshire':
+        print 'THIS IS WILTSHIRE2'
+    if 'wilt' in i['id']:
+        print 'CONTAINS WILT'
+    print record
+    scraperwiki.sqlite.save(['id'], record)
